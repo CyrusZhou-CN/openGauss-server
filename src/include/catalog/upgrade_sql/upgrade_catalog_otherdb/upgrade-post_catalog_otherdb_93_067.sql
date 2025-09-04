f2a8732ba82d57f261bcf1f6662af2885ad1be64
nnuanyang <zhaoxl@vastdata.com.cn>
2025-09-04 10:46:12 +0800

    修改C函数

--- src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade-post_catalog_otherdb_93_067.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade-post_catalog_otherdb_93_067.sql
@@ -3,14 +3,14 @@ DROP FUNCTION IF EXISTS pg_catalog.timezone(interval, timestamp without time zon
 SET LOCAL inplace_upgrade_next_system_object_oids=IUO_PROC, 2069;
 CREATE FUNCTION pg_catalog.timezone(text, timestamp without time zone)
 RETURNS timestamp without time zone
-AS 'select pg_catalog.timezone($1, $2::timestamptz)'
-LANGUAGE SQL
+AS 'timestamp_zone'
+LANGUAGE INTERNAL
 IMMUTABLE STRICT COST 1;
 COMMENT ON FUNCTION pg_catalog.timezone(text, timestamp without time zone) IS 'adjust timestamp to new time zone';
 SET LOCAL inplace_upgrade_next_system_object_oids=IUO_PROC, 2070;
 CREATE FUNCTION pg_catalog.timezone(interval, timestamp without time zone)
 RETURNS timestamp without time zone
-AS 'select pg_catalog.timezone($1, $2::timestamptz)'
-LANGUAGE SQL
+AS 'timestamp_izone'
+LANGUAGE INTERNAL
 IMMUTABLE STRICT COST 1;
 COMMENT ON FUNCTION pg_catalog.timezone(interval, timestamp without time zone) IS 'adjust timestamp to new time zone';
